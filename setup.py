88f884fe24ce30b09b44e1c790e4d2cdf0481adc
Nathan Cahill <nathan@nathancahill.com>
2015-08-26 14:46:24 -0600

    test reqs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='Flask-Inputs',
     version='0.0.1',
-    description='Incoming request data validation',
+    description='Flask request data validation',
     long_description=long_description,
     url='https://github.com/nathancahill/flask-inputs',
     author='Nathan Cahill',
@@ -21,4 +21,5 @@ setup(
     keywords='flask validation wtforms',
     py_modules=['flask_inputs'],
     install_requires=['flask', 'wtforms'],
+    tests_require=['nose', 'coverage', 'jsonschema'],
 )
